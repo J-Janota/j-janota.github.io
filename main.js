23bc4c681d2e5f3db86be434d939fcdca8606606
J-Janota <h.janota@hotmail.com>
2025-04-28 14:14:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 // First, register the Service Worker
 if ('serviceWorker' in navigator && 'PushManager' in window) {
-    navigator.serviceWorker.register('/https://j-janota.github.io/sw.js')
+    navigator.serviceWorker.register('sw.js')
       .then(registration => {
         console.log('Service Worker registered!', registration);
   
